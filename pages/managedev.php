e0cfee81b6af5fa29545463a661b03391af5306a
Jayant <jayantjnp@gmail.com>
2017-04-05 18:29:42 +0530

    debugging

--- pages/managedev.php
+++ pages/managedev.php
@@ -212,9 +212,9 @@ if($updatedev!=null and $updateswi!=null)//perform the updation task
 	if($updatedev==0 and $updateswi==0){
 		display();
 	}
-		echo $updatedev." switch ".$updateswi." groupid ".$gid." dname ".$dname;
 
 	if($gid!=null and $dname!=null and ($updatedev!=0 and $updateswi!=0)){//update only if both fields are not empty
+		echo $updatedev." switch ".$updateswi." groupid ".$gid." dname ".$dname;
 		$query="SELECT name FROM groups WHERE id='$gid'";
 		$grps=mysql_query($query);
 		$grp=mysql_fetch_assoc($grps);
